66f652d771c7876f68511fc6b9dffd6842d793b5
Javis <javischen9548@foxmail.com>
2022-02-15 17:55:16 +0800

    u

--- kt-cloud-gateway-acl/src/main/java/com/kt/cloud/gateway/acl/AccessApiFacade.java
+++ kt-cloud-gateway-acl/src/main/java/com/kt/cloud/gateway/acl/AccessApiFacade.java
@@ -4,9 +4,9 @@ import com.kt.cloud.iam.api.access.AccessApi;
 import com.kt.cloud.iam.api.access.request.ApiAccessRequest;
 import com.kt.cloud.iam.api.access.response.ApiAccessResponse;
 import com.kt.component.microservice.rpc.util.RpcUtils;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class AccessApiFacade {
 
     private final AccessApi accessApi;
