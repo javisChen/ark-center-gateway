2dc0f911d90c6be3001aa89fd638649e4cc961b4
Javis <javischen9548@foxmail.com>
2023-09-14 17:33:27 +0800

    u

--- ark-center-gateway-start/src/main/java/com/ark/center/gateway/GatewayApplication.java
+++ ark-center-gateway-start/src/main/java/com/ark/center/gateway/GatewayApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication()
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = {"com.ark.center.iam.api.*"})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
